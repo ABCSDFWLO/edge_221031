49a3ec918a044511a82aa379c69d5d470b908284
ABCSDFWLO <76253761+ABCSDFWLO@users.noreply.github.com>
2022-11-12 18:05:03 +0000

    아무것도 바뀐 게 없다고 자랑스럽게 말할 수 있다

---
+++ script.js
@@ -0,0 +1,11 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+let raf=null;
+
+window.addEventListener('resize', function() {
+  canvas.width = window.innerWidth;
+  canvas.height = window.innerHeight;
+});
+function draw(){
+  
+}
